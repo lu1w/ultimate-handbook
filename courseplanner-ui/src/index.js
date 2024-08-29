49c4524fb664b624aaf5cbb87f1c4d43df13d89c
cindyTAT <wanchengt@student.unimelb.edu.au>
2024-08-29 15:42:06 +1000

    feat: create subject card

--- courseplanner-ui/src/index.js
+++ courseplanner-ui/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CourseCard from './components/CourseCard';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Container, Row, Col } from 'react-bootstrap';
+import courseData from './mock-data/courseData'; // Import the dictionary
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Container>
+    <Row>
+      {courseData.map((course, index) => (
+        <Col md={4} key={index}>
+          <CourseCard type={course.type} code = {course.code} level = {course.level}
+                      name={course.name} points={course.points} />
+        </Col>
+      ))}
+    </Row>
+  </Container>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ courseplanner-ui/src/mock-data/courseData.js
@@ -0,0 +1,9 @@
+const courseData = [
+    { type: 'COMPULSORY', code: 'SCIE10005', level: '1', name: "Today's Science, Tomorrow's World", points: '12.5' },
+    { type: 'DISCIPLINE', code: 'MAST10006', level: '1', name: 'Calculus 2', points: '12.5' },
+    { type: 'MAJOR CORE', code: 'COMP30022', level: '3', name: 'IT Project', points: '12.5' },
+    { type: 'NONE', code: 'randomCode', level: '0', name: 'Not valid subject', points: '12.5' },
+  ];
+  
+  export default courseData;
+  
