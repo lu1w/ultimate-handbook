202e8a8d16c79a9fd176c0d7de3e0b3fe3285a04
Yilu W <luwyl@yahoo.com>
2024-09-08 14:44:36 +1000

    fix: organize react components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
-import courseData from './mock-data/courseData'; // Import the dictionary
-import CourseCard from './components/Common/CourseCard';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Container>
-    <Row>
-      {courseData.map((course, index) => (
-        <Col md={4} key={index}>
-          <CourseCard type={course.type} code = {course.code} level = {course.level}
-                      name={course.name} points={course.points} term = {course.term}/>
-        </Col>
-      ))}
-    </Row>
-  </Container>
+  <App />
 );
