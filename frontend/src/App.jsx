01d657e371c5eea8fe623886d4b96c2e8ea03f27
Yilu W <luwyl@yahoo.com>
2024-09-08 14:35:57 +1000

    chore: move config file and LICENSE to the root directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import SearchBar from "./components/Search/SearchBar";
 
 function App() {
-    return <div>
-        <SearchBar /> 
+  return <div>
+    <Container>
+      <Row>
+        {courseData.map((course, index) => (
+          <Col md={4} key={index}>
+            <CourseCard type={course.type} code = {course.code} level = {course.level}
+                        name={course.name} points={course.points} term = {course.term}/>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+    <SearchBar /> 
     </div>
 }
 
