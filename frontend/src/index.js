473835417bd26a8617ac721135b24b9307c8b9af
Yilu W <luwyl@yahoo.com>
2024-09-08 13:43:35 +1000

    chore: rename folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CourseCard from './components/CourseCard';
+import './index.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import courseData from './mock-data/courseData'; // Import the dictionary
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <App />
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Container>
+    <Row>
+      {courseData.map((course, index) => (
+        <Col md={4} key={index}>
+          <CourseCard type={course.type} code = {course.code} level = {course.level}
+                      name={course.name} points={course.points} term = {course.term}/>
+        </Col>
+      ))}
+    </Row>
+  </Container>
 );
