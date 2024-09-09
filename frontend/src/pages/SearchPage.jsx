1d7a212068fdc5fe693bd07253d22e842c3cc8b4
Yilu W <luwyl@yahoo.com>
2024-09-09 12:08:47 +1000

    feat: add pages component

---
+++ frontend/src/pages/SearchPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchBar from "../components/Search/SearchBar";
+
+function SearchPage() {
+  return <div>
+    <SearchBar /> 
+  </div>
+}
+
+export default SearchPage; 
