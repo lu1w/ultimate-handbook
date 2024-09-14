51c9d91024dbec54136d1dda74898c43e3ab6d7a
Yilu W <luwyl@yahoo.com>
2024-09-14 20:12:56 +1000

    feat: add button component and lint the frontend code

--- frontend/src/components/Common/CourseCard.jsx
+++ frontend/src/components/Common/CourseCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'; // Import Button component
+import PropTypes from 'prop-types';
 import '../../styles/CourseCard.css';
 
 const typeColors = {
@@ -9,7 +10,7 @@ const typeColors = {
   'MAJOR CORE': 'LightBlue',
 };
 
-const CourseCard = ({ type, code, level, points, name, term = [], state }) => {
+const CourseCard = ({ type, code, level, points, name, term = [] }) => {
   const headerBackgroundColor = typeColors[type] || 'defaultColor';
   const headerStyle = {
     backgroundColor: headerBackgroundColor,
@@ -17,23 +18,41 @@ const CourseCard = ({ type, code, level, points, name, term = [], state }) => {
 
   return (
     <Card className="course-card" border="black">
-      <Card.Header style={headerStyle} className="d-flex justify-content-between align-items-center">
+      <Card.Header
+        style={headerStyle}
+        className="d-flex justify-content-between align-items-center"
+      >
         <span>{type}</span>
-        <Button variant="outline-secondary" className="close-button">×</Button>
+        <Button variant="outline-secondary" className="close-button">
+          ×
+        </Button>
       </Card.Header>
       <Card.Body>
-        <Card.Text>{code} | Level {level} | {points} points</Card.Text>
+        <Card.Text>
+          {code} | Level {level} | {points} points
+        </Card.Text>
         <Card.Title>{name}</Card.Title>
-        
+
         {/* Render terms in a 2-column flex grid */}
         <div className="term-buttons">
           {term.map((termName, index) => (
-            <Button variant = "secondary" key={index} className="term-button">{termName}</Button>
+            <Button variant="secondary" key={index} className="term-button">
+              {termName}
+            </Button>
           ))}
         </div>
       </Card.Body>
     </Card>
   );
+};
+// Add prop-types validation for the props
+CourseCard.propTypes = {
+  type: PropTypes.string,
+  code: PropTypes.string,
+  level: PropTypes.string,
+  points: PropTypes.string,
+  name: PropTypes.string,
+  term: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default CourseCard;
