86966ffcae0365e9958060f8b6d2986648375040
ccIsCool6 <jinchenr@student.unimelb.edu.au>
2024-09-18 22:25:17 +1000

    chore: fix issues mentioned by Lucy

--- backend/src/routes/v1/course.route.js
+++ backend/src/routes/v1/course.route.js
@@ -2,19 +2,26 @@ const express = require('express');
 const router = express.Router();
 const mongoClient = require('../../config/mongoClient');
 const ApiError = require('../../utils/ApiError');
-const coursePlannerArray = {};
-
-// Get all subjects
-router.get('/allSubjects', async (req, res) => {
-  try {
-    const collection = await mongoClient.getCollection('Subjects');
-    const subjects = await collection.find({}).toArray();  // Get all subjects
-    res.json(subjects);  
-  } catch (err) {
-    console.error('Error retrieving data:', err);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
+const coursePlanner = {};
+//Example of coursePlanner:
+// {
+//   "coursePlannerArray": {
+//     "2024s1": {
+//       "p1": {
+//         "subjectCode": "COMP10002",
+//         "subjectName": "COMPUTER!",
+//         "more": "..."
+//       }
+//     },
+//     "2024s2": {
+//       "p2": {
+//         "subjectCode": "COMP10002",
+//         "subjectName": "COMPUTER!",
+//         "more": "..."
+//       }
+//     }
+//   }
+// }
 
 
 router.get('/', (req, res) => {
@@ -22,55 +29,55 @@ router.get('/', (req, res) => {
         message: 'Welcome to the U handbook!!!.' 
     });
 });
-// Remove one course from one slot
+// Remove one subject from one slot
 router.delete('/remove/:query', async(req, res, next) => {
   const { query } = req.params;
   if (!query) {
-      return next(new ApiError(400, 'No course data provided.'));
+      return next(new ApiError(400, 'No subject data provided.'));
   }
 
   const semesterKey = query.substring(0, 6); // '2024s2 '
   const position = query.substring(6, 8); // 'p1'
 
-  // check if course exists
-  if (!coursePlannerArray[semesterKey] || !coursePlannerArray[semesterKey][position]) {
-      return res.status(404).json({ message: 'No course found!' });
+  // check if subject exists
+  if (!coursePlanner[semesterKey] || !coursePlanner[semesterKey][position]) {
+      return res.status(404).json({ message: 'No subject found!' });
   }
 
-  delete coursePlannerArray[semesterKey][position];
+  delete coursePlanner[semesterKey][position];
 
   res.json({ 
-      message: 'Successfully removed course!',
-      coursePlannerArray: coursePlannerArray
+      message: 'Successfully removed subject!',
+      coursePlanner: coursePlanner
   });
 });
-// Drag one course to one slot
+// Drag one subject to one slot
 router.post('/add', async(req, res, next) => {
-  const courseData = req.body; // e.g. { "2024s21": { course } }
-  if (!courseData || Object.keys(courseData).length === 0) {
-      return res.status(400).json({ message: 'No course data provided.' });
+  const subjectData = req.body; // e.g. { "2024s21": { subject } }
+  if (!subjectData || Object.keys(subjectData).length === 0) {
+      return res.status(400).json({ message: 'No subject data provided.' });
   }
 
-  const param = Object.keys(courseData)[0]; //e.g.'2024s21'
-  const course = courseData[param]; // get the course object
+  const param = Object.keys(subjectData)[0]; //e.g.'2024s21'
+  const subject = subjectData[param]; // get the subject object
 
   const time = param.substring(0, 6); // '2024s2'
   const position = param.substring(6, 8); // 'p1'
 
   // intialize the semester if it doesn't exist
-  if (!coursePlannerArray[time]) {
-      coursePlannerArray[time] = {};
+  if (!coursePlanner[time]) {
+      coursePlanner[time] = {};
   }
 
-  // add course to the planner
-  if (coursePlannerArray[time][position]) {
-      return res.status(400).json({ message: 'can not add course to this slot!' });
+  // add subject to the planner
+  if (coursePlanner[time][position]) {
+      return res.status(400).json({ message: 'can not add subject to this slot!' });
   }
-  coursePlannerArray[time][position] = course;
+  coursePlanner[time][position] = subject;
 
   res.json({ 
-      message: 'successfully added course!',
-      coursePlannerArray: coursePlannerArray
+      message: 'successfully added subject!',
+      coursePlanner: coursePlanner
   });
 });
 
@@ -83,11 +90,11 @@ router.get("/subject/prerequisite/:query", async (req, res) => {
     if (query) {
         try {
             const collection = await mongoClient.getCollection('Subject');
-            const course = await collection.findOne({ subjectCode: query });
-            if (!course) {
+            const subject = await collection.findOne({ subjectCode: query });
+            if (!subject) {
                 return next(new ApiError(404, 'Course not found.'));
             }
-            return res.json(course.prerequisites);
+            return res.json(subject.prerequisites);
         } catch (err) {
             console.error('Error:', err);
             return next(new ApiError(500, 'Server error'));
@@ -110,20 +117,20 @@ router.get('/majorCompulsory', async (req, res, next) => {
     //await console.log('major:', major);
     const collectionSubjects = await mongoClient.getCollection('Subjects');
 
-    // // Suppose every course has a field 'prerequisiteFor' that contains the major it is a prerequisite for
+    // // Suppose every subject has a field 'prerequisiteFor' that contains the major it is a prerequisite for
 
     const majorCourses = collectionSubjects.find(major);// 但这里是找到所有相关major的课程不是找到major必修的课程
 
     // Get all the prerequisites for the major's core courses
     let corePrerequisitesOfMajor = [];
 
-    majorCourses.forEach(course => {
-      if (course && course.prerequisites.length > 0 && course.isCompulsory) {
+    majorCourses.forEach(subject => {
+      if (subject && subject.prerequisites.length > 0 && subject.isCompulsory) {
 
           corePrerequisitesOfMajor.push({
-          subjectName: course.subjectName,
-          subjectCode: course.subjectCode,
-          prerequisites: course.prerequisites
+          subjectName: subject.subjectName,
+          subjectCode: subject.subjectCode,
+          prerequisites: subject.prerequisites
           
         });
       }
