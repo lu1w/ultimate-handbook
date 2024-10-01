cd96963b9236084bfcf35306dd566fff048c1631
ccIsCool6 <jinchenr@student.unimelb.edu.au>
2024-10-01 14:38:57 +1000

    chore: fix frontend validation error

--- frontend/src/components/Common/EmptyCourseCard.jsx
+++ frontend/src/components/Common/EmptyCourseCard.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'; // Import Button component
 import '../../styles/EmptyCourseCard.css';
-
+import PropTypes from 'prop-types'; // Import prop types
 const EmptyCourseCard = ({ onAdd }) => {
   return (
     <Card className="empty-course-card" border="black">
       <Card.Body className="d-flex justify-content-center align-items-center">
-        <Button variant="outline-secondary" className="add-button" onClick={onAdd}>
+        <Button
+          variant="outline-secondary"
+          className="add-button"
+          onClick={onAdd}
+        >
           <img src="/plus.svg" alt="Add" className="plus-icon" />
         </Button>
       </Card.Body>
@@ -15,4 +19,8 @@ const EmptyCourseCard = ({ onAdd }) => {
   );
 };
 
+// add prop types for EmptyCourseCard
+EmptyCourseCard.propTypes = {
+  onAdd: PropTypes.func.isRequired, // onAdd is a function and is required
+};
 export default EmptyCourseCard;
