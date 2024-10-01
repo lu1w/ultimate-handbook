7f4e8d44b6650ceda0e26fccab51fc8ffe0cc562
ccIsCool6 <jinchenr@student.unimelb.edu.au>
2024-10-01 19:52:53 +1000

    chore(UH-67): we have auto test action now

--- backend/test/search.route.test.mjs
+++ backend/test/search.route.test.mjs
@@ -2,7 +2,6 @@
 import request from 'supertest'; 
 import { expect } from 'chai';
 import app from '../src/app.js'; 
-
 describe("Search Routes", () => {
   
   it("should search for a subject by query", (done) =>{
