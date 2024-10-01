7e74fe1e5bee9b2414b4c2b7a0c923d54bf25c74
ccIsCool6 <jinchenr@student.unimelb.edu.au>
2024-10-01 19:24:38 +1000

    fix: timeout increase to 5000ms

--- backend/test/search.route.test.mjs
+++ backend/test/search.route.test.mjs
@@ -2,9 +2,11 @@ import request from 'supertest';
 import { expect } from 'chai'; 
 import app from '../src/app.js'; 
 
+
 describe("Search Routes", () => {
   
-  it("should search for a subject by query", (done) => {
+  it("should search for a subject by query", function (done)  {
+    this.timeout(5000); // set timeout to 5 seconds
     const query = "COMP10002"; // search for COMP10002
     request(app)
       .get(`/v1/search/subject/${query}`) // GET request
@@ -21,8 +23,17 @@ describe("Search Routes", () => {
 
         // make sure the subejct structure is correct
         if (res.body.subjects.length > 0) {
-          expect(res.body.subjects[0]).to.have.property("subjectName");
-          expect(res.body.subjects[0]).to.have.property("subjectCode");
+          const subject = res.body.subjects[0];
+          expect(subject).to.have.property("_id");
+          expect(subject).to.have.property("subjectName").that.is.a("string");
+          expect(subject).to.have.property("subjectCode").that.is.a("string");
+          expect(subject).to.have.property("level").that.is.a("number");
+          expect(subject).to.have.property("points").that.is.a("number");
+          expect(subject).to.have.property("location");
+          expect(subject).to.have.property("subjectUrl").that.is.a("string");
+          expect(subject).to.have.property("prerequisites").that.is.an("array");
+          expect(subject).to.have.property("corequisites").that.is.an("array");
+          expect(subject).to.have.property("nonAllowedSubjects").that.is.an("array");
         }
 
         done(); 
