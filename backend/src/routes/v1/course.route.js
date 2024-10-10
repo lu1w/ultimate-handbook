6da91163690f558243a2aba62f8674476496fb45
ccIsCool6 <jinchenr@student.unimelb.edu.au>
2024-10-11 00:59:59 +1100

    chore: delete unused comments

--- backend/src/routes/v1/course.route.js
+++ backend/src/routes/v1/course.route.js
@@ -9,35 +9,6 @@ const {
   isValidAdd,
   giveTypeOfSubject
 } = require("../../service/courseService");
-
-// const SubjectsPlanner = {};
-// const userDegree = {
-//   degree: "",
-//   major: ""
-// };
-// const compulsory = []
-// const majorCore = [];
-
-//Example of SubjectsPlanner:
-// {
-//   "SubjectsPlannerArray": {
-//     "2024s1": {
-//       "p1": {
-//         "SubjectsCode": "COMP10002",
-//         "SubjectsName": "COMPUTER!",
-//         "more": "..."
-//       }
-//     },
-//     "2024s2": {
-//       "p2": {
-//         "SubjectsCode": "COMP10002",
-//         "SubjectsName": "COMPUTER!",
-//         "more": "..."
-//       }
-//     }
-//   }
-// }
-
 /**
  * @swagger
  * /course/main:
@@ -80,69 +51,7 @@ const {
  *       500:
  *         description: Server error
  */
-router.get("/main", getInitialInformation, (req, res) => {
-  const { userDegree, compulsory, majorCore } = res.locals;
-
-  res.json({
-    message: "Core subjects and compulsory courses retrieved successfully",
-    userDegree,
-    coreSubjects: majorCore,
-    compulsorySubject: compulsory
-  });
-});
-
-// router.get("/main", async (req, res, next) => {
-//   try {
-//     const { majorName, degree } = req.query;
-
-//     // Validate that both major and degree are provided
-//     if (!majorName || !degree) {
-//       return next(new ApiError(400, "Both majorName and degree are required."));
-//     }
-
-//     // Store degree and major in userDegree object
-//     userDegree.degree = degree;
-//     userDegree.major = majorName;
-
-//     // Fetch core subjects based on the major
-//     const majorCollection = await mongoClient.getCollection("Major");
-//     const majorInfo = await majorCollection.findOne({ majorName: majorName });
-
-//     if (!majorInfo) {
-//       return next(new ApiError(404, "Major not found."));
-//     }
-
-//     const { coreSubjects } = majorInfo;
-
-//     // Store core subjects in majorCore array
-//     majorCore.length = 0; // Clear any existing data
-//     majorCore.push(...coreSubjects);
-
-//     // Fetch compulsory courses based on the degree
-//     const degreeCollection = await mongoClient.getCollection("Course");
-//     const degreeInfo = await degreeCollection.findOne({ courseName: degree });
-
-//     if (!degreeInfo) {
-//       return next(new ApiError(404, "Degree not found."));
-//     }
-
-//     const { major } = degreeInfo;
-
-//     // Store compulsory courses in compulsory array
-//     compulsory.length = 0; // Clear any existing data
-//     compulsory.push(...major);
-
-//     // Respond with both core subjects and compulsory courses
-//     res.json({
-//       message: "Core subjects and compulsory courses retrieved successfully",
-//       coreSubjects: majorCore,
-//       compulsorySubject: compulsory
-//     });
-//   } catch (err) {
-//     console.error("Error:", err);
-//     return next(new ApiError(500, "Server error"));
-//   }
-// });
+router.get("/main", getInitialInformation);
 
 /**
  * @swagger
@@ -165,68 +74,9 @@ router.get("/main", getInitialInformation, (req, res) => {
  *       400:
  *         description: inavlid query!
  */
-// Remove one Subject from one slot
-// router.delete("/remove/:query", async (req, res, next) => {
-//   const { query } = req.params;
-//   if (!query) {
-//     return next(new ApiError(400, "No Subject data provided."));
-//   }
 
-//   const semesterKey = query.substring(0, 6); // '2024s2 '
-//   const position = query.substring(6, 8); // 'p1'
-
-//   // check if Subject exists
-//   if (
-//     !SubjectsPlanner[semesterKey] ||
-//     !SubjectsPlanner[semesterKey][position]
-//   ) {
-//     return res.status(404).json({ message: "No Subjects found!" });
-//   }
-
-//   delete SubjectsPlanner[semesterKey][position];
-
-//   res.json({
-//     message: "Successfully removed Subjects!",
-//     SubjectsPlanner: SubjectsPlanner
-//   });
-// });
 router.delete("/remove/:query", removeSubject);
 
-// function arePrerequisitesMet(prerequisites, subjectsCodeInPlanner) {
-//   // prerequisites 是一个数组的数组
-//   // subjectsCodeInPlanner 是已完成的课程代码数组
-//   console.log("Here is all prerequisite of subjects" ,prerequisites);
-
-//   for (const orGroup of prerequisites) {
-//     // orGroup 是一个课程代码数组，只需满足其中一门课程
-//     let groupSatisfied = true;
-//     for (const subjectCode of orGroup) {
-//       if (!subjectsCodeInPlanner.includes(subjectCode)) {
-//         groupSatisfied = false;
-//         break;
-//       }
-//     }
-//     if (groupSatisfied) {
-//       // 如果有一个组不满足，先修条件就不满足
-//       return true;
-//     }
-//   }
-//   console.log("Here is all subjects in planner now" ,subjectsCodeInPlanner);
-//   return false;
-// }
-
-// function getAllSubjectCodes(SubjectsPlanner) {
-//   const subjectCodesArray = [];
-//   for (const semester in SubjectsPlanner) {
-//     for (const position in SubjectsPlanner[semester]) {
-//       const subject = SubjectsPlanner[semester][position];
-//       if (subject && subject.subjectCode) {
-//         subjectCodesArray.push(subject.subjectCode);
-//       }
-//     }
-//   }
-//   return subjectCodesArray;
-// }
 /**
  * @swagger
  * /course/add:
@@ -247,89 +97,6 @@ router.delete("/remove/:query", removeSubject);
  *       400:
  *         description: failed to add Subject
  */
-// Drag one Subjects to one slot
-// router.post("/add", async (req, res) => {
-//   const SubjectsData = req.body; // e.g. { "2024s21": { Subjects } }
-//   if (!SubjectsData || Object.keys(SubjectsData).length === 0) {
-//     return res.status(400).json({ message: "No Subjects data provided." });
-//   }
-
-//   const param = Object.keys(SubjectsData)[0]; //e.g.'2024s21'
-//   const Subjects = SubjectsData[param]; // get the Subjects object
-
-//   const time = param.substring(0, 6); // '2024s2'
-//   const position = param.substring(6, 8); // 'p1'
-
-//   // intialize the semester if it doesn't exist
-//   if (!SubjectsPlanner[time]) {
-//     SubjectsPlanner[time] = {};
-//   }
-
-//   // add Subjects to the planner
-//   if (SubjectsPlanner[time][position]) {
-//     return res
-//       .status(400)
-//       .json({ message: "can not add Subjects to this slot!,because subject already exist" });
-//   }
-
-//   const { subjectCode } = Subjects;
-//   if (!subjectCode) {
-//     return res.status(400).json({ message: "lack of the subject code" });
-//   }
-
-//   // 在添加课程之前，检查先修课程是否满足
-//   const subjectsCodeInPlanner = getAllSubjectCodes(SubjectsPlanner);
-//   const { prerequisites } = Subjects;
-//   console.log("INFO 3 ", prerequisites);
-//   if (prerequisites && prerequisites.length > 0) {
-//     const prerequisitesMet = arePrerequisitesMet(prerequisites, subjectsCodeInPlanner);
-//     console.log("INFO 1 ", prerequisitesMet);
-//     if (!prerequisitesMet) {
-//       return res.status(400).json({ message: "the prerequisties of this subject is not satisified" });
-//     }
-//   }
-//   console.log("INFO 2", prerequisites);
-
-//   // check if the Subjects is compulsory, core, discipline or breadth
-//   if (compulsory.includes(subjectCode)) {
-//     Subjects.type = 'compulsory';
-//   } else if (majorCore.includes(subjectCode)) {
-//     Subjects.type = 'core';
-//   } else {
-//     // get the first 4 characters of the Subjects code
-//     const codePrefix = subjectCode.substring(0, 4).toUpperCase();
-
-//     try {
-//       const collection = await mongoClient.getCollection('StudyAreaToCourse');
-//       const studyAreaDoc = await collection.findOne({});
-
-//       if (studyAreaDoc && studyAreaDoc[codePrefix]) {
-//         const degreeNameForPrefix = studyAreaDoc[codePrefix]; // get the degree name for the prefix(e.g. MAST)
-//         // check if the Subjects is discipline or breadth
-//         const userDegreeName = userDegree.degree;
-
-//         if (degreeNameForPrefix === userDegreeName) {
-//           Subjects.type = 'discipline';
-//         } else {
-//           Subjects.type = 'breadth';
-//         }
-//       } else {
-//         Subjects.type = 'breadth';
-//       }
-//     } catch (err) {
-//       console.error("error:", err);
-//       return next(new ApiError(500, "server error"));
-//     }
-//   }
-
-//   SubjectsPlanner[time][position] = Subjects;
-
-//   res.json({
-//     message: "successfully added Subjects!",
-//     SubjectsPlanner: SubjectsPlanner
-//   });
-
-// });
 router.post("/add", addSubject, isValidAdd, giveTypeOfSubject);
 
 /**
--- backend/src/service/courseService.js
+++ backend/src/service/courseService.js
@@ -1,7 +1,5 @@
 const mongoClient = require("../config/mongoClient");
 const ApiError = require("../utils/ApiError");
-// 如果 subjectPlanner 是需要在整个应用中共享的数据，我们需要确保它在不同的模块中可访问。
-// 可以将 subjectPlanner 导出，以便在其他文件中共享。
 const subjectPlanner = {};
 const userDegree = {
   degree: "",
@@ -13,25 +11,20 @@ const majorCore = [];
 const getInitialInformation = async (req, res, next) => {
   try {
     const { majorName, degree } = req.query;
-
     if (!majorName || !degree) {
       return next(new ApiError(400, "Both majorName and degree are required."));
     }
+    userDegree.degree = degree;
+    userDegree.major = majorName;
 
-    // store data in locals
-    res.locals.userDegree = {
-      degree,
-      major: majorName
-    };
     // Fetch core subjects based on the major
     const majorCollection = await mongoClient.getCollection("Major");
     const majorInfo = await majorCollection.findOne({ majorName: majorName });
-
     if (!majorInfo) {
       return next(new ApiError(404, "Major not found."));
     }
     const { coreSubjects } = majorInfo;
-    res.locals.majorCore = coreSubjects;
+    majorCore.push(...coreSubjects);
 
     // Fetch compulsory courses based on the degree
     const degreeCollection = await mongoClient.getCollection("Course");
@@ -40,10 +33,14 @@ const getInitialInformation = async (req, res, next) => {
       return next(new ApiError(404, "Degree not found."));
     }
     const { compulsorySubject } = degreeInfo;
-    res.locals.compulsory = compulsorySubject;
+    compulsory.push(...compulsorySubject);
 
-    // 调用下一个中间件或路由处理器
-    next();
+    res.json({
+      message: "Core subjects and compulsory courses retrieved successfully",
+      userDegree,
+      coreSubjects: majorCore,
+      compulsorySubject: compulsory
+    });
   } catch (err) {
     console.error("Error:", err);
     return next(new ApiError(500, "Server error"));
@@ -77,57 +74,6 @@ const addSubject = async (req, res, next) => {
       return res.status(400).json({ message: "lack of the subject code" });
     }
     subjectPlanner[time][position] = Subject;
-
-    // const subjectsCodeInPlanner = getAllSubjectCodes(subjectPlanner)
-    // checkAllSubjectPrequisites(subjectsCodeInPlanner);// we will check all subjects prerequisites in planner after adding the subject
-
-    // // check if the Subjects is in the right semester
-    // const {studyPeriod} = Subject;
-    // // Check if studyPeriod array contains any items
-    // if (studyPeriod && studyPeriod.length > 0) {
-    //     // Loop through each study period to check against the subjectSemesterInPlanner
-    //     const match = studyPeriod.some(period => {
-    //         const studyRequireSemester = period.substring(9); // Extract subjectSemesterInPlanner number
-    //         return studyRequireSemester === subjectSemesterInPlanner;
-    //     });
-    //     if(!match){
-    //         Subject.semesterError = "not in right semester position!";
-    //     }
-    //     else{
-    //         delete Subject.semesterError;
-    //     }
-    // }
-
-    // check if the Subjects is compulsory, core, discipline or breadth
-    // if (compulsory.includes(subjectCode)) {
-    //   Subject.type = 'compulsory';
-    // } else if (majorCore.includes(subjectCode)) {
-    //   Subject.type = 'core';
-    // } else {
-    //   // get the first 4 characters of the Subjects code
-    //   const codePrefix = subjectCode.substring(0, 4).toUpperCase();
-    //   try {
-    //     const collection = await mongoClient.getCollection('StudyAreaToCourse');
-    //     const studyAreaDoc = await collection.findOne({});
-
-    //     if (studyAreaDoc && studyAreaDoc[codePrefix]) {
-    //       const degreeNameForPrefix = studyAreaDoc[codePrefix]; // get the degree name for the prefix(e.g. MAST)
-    //       // check if the Subjects is discipline or breadth
-    //       const userDegreeName = userDegree.degree;
-
-    //       if (degreeNameForPrefix === userDegreeName) {
-    //         Subject.type = 'discipline';
-    //       } else {
-    //         Subject.type = 'breadth';
-    //       }
-    //     } else {
-    //       Subject.type = 'breadth';
-    //     }
-    //   } catch (err) {
-    //     console.error("error:", err);
-    //     return next(new ApiError(500, "server error"));
-    //   }
-    // }
     next();
   } catch (err) {
     console.error("Error:", err);
@@ -243,7 +189,7 @@ const removeSubject = (req, res, next) => {
   }
 };
 
-// 这个函数会决定是否给courseplanner里某一门课加上prerequisteError
+// this function will decide whether need to add error to the subject
 function checkAllSubjectPrequisites(subjectsCodeInPlanner) {
   // loop all subjects in the subjectPlanner
   for (const semester in subjectPlanner) {
@@ -280,12 +226,12 @@ function getAllSubjectCodes(subjectPlanner) {
 }
 
 function arePrerequisitesMet(prerequisites, subjectsCodeInPlanner) {
-  // prerequisites 是一个数组的数组
-  // subjectsCodeInPlanner 是已完成的课程代码数组
+  // prerequisites are an array of arrays
+  // subjectsCodeInPlanner is an array of subject codes
   console.log("Here is all prerequisite of subjects", prerequisites);
 
   for (const andGroup of prerequisites) {
-    // andGroup 是一个课程代码数组，需要全部满足
+    // andGroup is an array of subject codes which need to be all satisfied
     let groupSatisfied = false;
     for (const subjectCode of andGroup) {
       if (subjectsCodeInPlanner.includes(subjectCode)) {
@@ -294,7 +240,7 @@ function arePrerequisitesMet(prerequisites, subjectsCodeInPlanner) {
       }
     }
     if (!groupSatisfied) {
-      // 如果有一个组满足，先修条件就满足
+      // if any group is not satisfied, return false
       return false;
     }
   }
@@ -306,9 +252,5 @@ module.exports = {
   isValidAdd,
   giveTypeOfSubject,
   removeSubject,
-  getInitialInformation,
-  subjectPlanner,
-  userDegree,
-  compulsory,
-  majorCore
+  getInitialInformation
 };
