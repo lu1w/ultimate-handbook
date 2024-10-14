d4e1d10d0fb43c482736bdc632dccda76c511569
ccIsCool6 <jinchenr@student.unimelb.edu.au>
2024-10-14 22:01:07 +1100

    chore: update the coursePlanner structure

--- backend/src/service/courseService.js
+++ backend/src/service/courseService.js
@@ -1,54 +1,78 @@
 const mongoClient = require('../config/mongoClient');
 const ApiError = require('../utils/ApiError');
 const subjectPlanner = {
+  y1su: {
+    p1: {},
+    p2: {}
+  },
   y1s1: {
     p1: {},
     p2: {},
     p3: {},
-    p4: {},
-    p5: {}
+    p4: {}
+  },
+  y1wi: {
+    p1: {},
+    p2: {}
   },
   y1s2: {
     p1: {},
     p2: {},
     p3: {},
-    p4: {},
-    p5: {}
+    p4: {}
+  },
+
+  y2su: {
+    p1: {},
+    p2: {}
   },
   y2s1: {
     p1: {},
     p2: {},
     p3: {},
-    p4: {},
-    p5: {}
+    p4: {}
+  },
+  y2wi: {
+    p1: {},
+    p2: {}
   },
   y2s2: {
     p1: {},
     p2: {},
     p3: {},
-    p4: {},
-    p5: {}
+    p4: {}
+  },
+
+  y3su: {
+    p1: {},
+    p2: {}
   },
   y3s1: {
     p1: {},
     p2: {},
     p3: {},
-    p4: {},
-    p5: {}
+    p4: {}
+  },
+  y3wi: {
+    p1: {},
+    p2: {}
   },
   y3s2: {
     p1: {},
     p2: {},
     p3: {},
-    p4: {},
-    p5: {}
+    p4: {}
+  },
+
+  y4su: {
+    p1: {},
+    p2: {}
   },
   y4s1: {
     p1: {},
     p2: {},
     p3: {},
-    p4: {},
-    p5: {}
+    p4: {}
   }
 };
 
