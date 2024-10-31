b7d6bad7b3edbd1ef05363dc437334aee1d99b00
Yilu W <luwyl@yahoo.com>
2024-10-31 21:16:59 +1100

    docs(UH-35): update progression documentation

--- backend/src/routes/v1/course.route.js
+++ backend/src/routes/v1/course.route.js
@@ -398,18 +398,75 @@ router.get('/cores/:major', async (req, res, next) => {
 
 /**
  * @swagger
- * /course/user/{userId}/pro`gressions:
+ * /course/user/{userId}/progressions:
  *   get:
  *     summary: get all the degree progression statistics
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *         description: user ID
+ *     responses:
+ *       200:
+ *         description: Successfully retrieve the progression status.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 progressions:
+ *                   type: object
+ *                   example:
+ *                     {
+ *                       general: {
+ *                         compulsory: { stats: 'Compulsory Subject: SCIE10005', fulfilled: true },
+ *                         breadth: {
+ *                           stats: '0 / 50(min) to 62.5(max) Credit Points of Breadth Subject',
+ *                           fulfilled: false,
+ *                         },
+ *                       },
+ *                       levelsRules: {
+ *                         overall: {
+ *                           level1: {
+ *                             stats: '0 / 125(max) Credit Points of Level 1 Subject',
+ *                             fulfilled: true,
+ *                           },
+ *                         },
+ *                         discipline: {
+ *                           level1: {
+ *                             stats: '0 / 62.5(min) Credit Points of Level 1 Discipline Subject',
+ *                             fulfilled: false,
+ *                           },
+ *                           level2: {
+ *                             stats: '0 / 62.5(min) Credit Points of Level 2 Discipline Subject',
+ *                             fulfilled: false,
+ *                           },
+ *                           level3: {
+ *                             stats: '0 / 75(min) Credit Points of Level 3 Discipline Subject',
+ *                             fulfilled: false,
+ *                           },
+ *                         },
+ *                         breadth: {
+ *                           level1: {
+ *                             stats: '0 / 25(max) Credit Points of Level 1 Breadth Subject',
+ *                             fulfilled: true,
+ *                           },
+ *                         },
+ *                         degreeProgression: {},
+ *                         distinctStudyArea: {},
+ *                       },
+ *                     };
+ *       500:
+ *         description: Server failure.
  */
 router.get('/user/:userId/progressions', async (req, res) => {
   const { userId } = req.params;
-  // try {
-  const progressions = await getProgressions(userId);
-  res.status(200).send({ progressions });
-  // } catch (err) {
-  //   // TODO: handle error
-  // }
+  try {
+    const progressions = await getProgressions(userId);
+    res.status(200).send({ progressions });
+  } catch (err) {
+    res.status(500).send({ message: `Server error: ${err}` });
+  }
 });
 
 module.exports = router;
