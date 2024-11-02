dce342e4bda6998ccbf017f8dbabfa06f17331d0
cindyTAT <wanchengt@student.unimelb.edu.au>
2024-11-02 18:15:01 +1100

    fix(UH-84): fix filter state error

--- frontend-nextjs/src/app/search/[userId]/page.tsx
+++ frontend-nextjs/src/app/search/[userId]/page.tsx
@@ -20,12 +20,12 @@ export default function SearchPage({ params }: { params: { userId: string } }) {
   const [result, setResult] = useState([]);
 
   /* Filter */
-  // const [levels, setLevels] = useState<Set<Level>>(new Set<Level>(allLevels));
-  // const [studyPeriods, setStudyPeriods] = useState<Set<StudyPeriod>>(
-  //   new Set<StudyPeriod>(allStudyPeriod),
-  // );
-  const levels = new Set<Level>(allLevels);
-  const studyPeriods = new Set<StudyPeriod>(allStudyPeriod);
+  const [levels, setLevels] = useState<Set<Level>>(new Set<Level>(allLevels));
+  const [studyPeriods, setStudyPeriods] = useState<Set<StudyPeriod>>(
+    new Set<StudyPeriod>(allStudyPeriod),
+  );
+  // const levels = new Set<Level>(allLevels);
+  // const studyPeriods = new Set<StudyPeriod>(allStudyPeriod);
   const [studyAreas, setStudyAreas] = useState<Set<string>>(new Set<string>());
 
   /* Fetch subject data when the component mounts */
